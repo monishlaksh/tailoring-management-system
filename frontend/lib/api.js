b404c805667641edba3131fd59498492ef6070ca
Monish <monishdurai29@gmail.com>
2026-05-29 09:56:15 +0530

    fix url

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: 'https://tailoring-management-apwh.onrender.com/',
   withCredentials: true,
 })
 
